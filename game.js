18c60aa983ab8eedcda53a166189892869de5091
bf-eng <58610837+bf-eng@users.noreply.github.com>
2024-09-26 16:11:05 -0700

    Add adConfig Call on Mute

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ class Game {
     // Define variables
     this.score = 0;
     this.choice = '';
+    this.muted = false;
 
     this.canvas = document.getElementById('gameContainer').getContext('2d');
     this.canvas.font = '24px Arial';
@@ -11,6 +12,11 @@ class Game {
     this.playButton = document.getElementById('playButton');
     this.headsButton = document.getElementById('headsButton');
     this.tailsButton = document.getElementById('tailsButton');
+    this.muteButton = document.getElementById('muteButton');
+
+    adConfig({
+      sound: 'on',
+    });
 
     // On click listeners for the game's buttons.
     this.playButton.addEventListener('click', () => {
@@ -26,6 +32,15 @@ class Game {
     this.tailsButton.addEventListener('click', () => {
       this.choice = 'Tails';
       this.flipCoin();
+    });
+
+    this.muteButton.addEventListener('click', () => {
+      var soundString = this.muted ? 'on' : 'off';
+      this.muteButton.innerHTML = this.muted ? 'Mute sound' : 'Un-mute sound';
+      this.muted = !this.muted;
+      adConfig({
+        sound: soundString,
+      });
     });
 
     this.erase();
