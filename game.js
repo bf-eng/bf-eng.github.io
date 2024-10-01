ec42ff60b549f3ba7c3b6d1cc131d6a621d4b028
bf-eng <58610837+bf-eng@users.noreply.github.com>
2024-10-01 14:47:37 -0700

    Add Rewarded Ads

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ class Game {
     this.choice = '';
     this.muted = false;
     this.shouldShowAdOnPlay = false;
+    this.showRewardedAdFn = null;
 
     this.canvas = document.getElementById('gameContainer').getContext('2d');
     this.canvas.font = '24px Arial';
@@ -14,6 +15,7 @@ class Game {
     this.headsButton = document.getElementById('headsButton');
     this.tailsButton = document.getElementById('tailsButton');
     this.muteButton = document.getElementById('muteButton');
+    this.continueButton = document.getElementById('continueButton');
 
     adConfig({
       sound: 'on',
@@ -44,6 +46,12 @@ class Game {
       });
     });
 
+    this.continueButton.addEventListener('click', () => {
+      if (this.showRewardedAdFn) {
+        this.showRewardedAdFn();
+      }
+    });
+
     this.erase();
   }
 
@@ -64,6 +72,7 @@ class Game {
     this.canvas.fillText('Score: ' + this.score, 8, 26);
     this.canvas.fillText('Heads or Tails?', 66, 150);
     this.playButton.style.display = 'none';
+    this.continueButton.style.display = 'none';
     this.headsButton.style.display = 'inline-block';
     this.tailsButton.style.display = 'inline-block';
   }
@@ -117,6 +126,33 @@ class Game {
     this.headsButton.style.display = 'none';
     this.tailsButton.style.display = 'none';
     this.shouldShowAdOnPlay = true;
+
+    adBreak({
+      type: 'reward',  // rewarded ad
+      name: 'reward-continue',
+      beforeReward: (showAdFn) => {
+        this.showRewardedAdFn = () => { showAdFn(); };
+        // Rewarded ad available - prompt user for a rewarded ad
+        this.continueButton.style.display = 'inline-block';
+      },
+      beforeAd: () => { this.disableButtons(); },     // You may also want to mute the game's sound.
+      adDismissed: () => {
+        this.continueButton.style.display = 'none';   // Hide the reward button and continue lose flow.
+      },
+      adViewed: () => { this.continueGame(); },       // Reward granted - continue game at current score.
+      afterAd: () => { this.enableButtons(); },       // Resume the game flow.
+    });
+  }
+
+  // Continue gameplay at current score
+  continueGame() {
+    this.erase();
+    this.canvas.fillText('Score: ' + this.score, 8, 26);
+    this.canvas.fillText('Heads or Tails?', 66, 150);
+    this.playButton.style.display = 'none';
+    this.continueButton.style.display = 'none';
+    this.headsButton.style.display = 'inline-block';
+    this.tailsButton.style.display = 'inline-block';
   }
 
   // Erase the canvas
